90188bf5bb0ef4358c83a540f038e50475d2c030
Talha <talhaerogluu35@gmail.com>
2022-08-30 21:23:38 +0300

    Initial commit of microservice project with react and node.

---
+++ client/src/PostCreate.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+    const [title, setTitle] = useState('');    
+   
+    const onSubmit = async (event) => {
+        event.preventDefault();
+    
+        await axios.post('http://192.168.100.10:4000/posts', {
+            title
+        });
+    
+        setTitle('');
+    };
+
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input 
+                        value={title} 
+                        onChange={e => setTitle(e.target.value)}
+                        className="form-control"/>
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        );
+};
+export default App;
